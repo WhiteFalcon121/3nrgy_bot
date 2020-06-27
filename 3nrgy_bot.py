528cd7ca6ab6f4ac464449cc9bf7732002660a6d
WhiteFalcon121 <karkau007@gmail.com>
2020-06-27 09:25:20 +0100

    discrim trial to implement later into trading

--- 3nrgy_bot.py
+++ 3nrgy_bot.py
@@ -97,10 +97,11 @@ async def howareyou(ctx):
     replies = ['A bit sad,', 'Ok, I guess,', 'Good,', 'Really happy,', "Don't bother me - I'm doing Karthik's Maths homework. Why do you think he does that well?", 'I feel human.']
     name = str(ctx.author)
     name = str(name[:-5])
+    discrim = ctx.author.discriminator
     randreply = random.choice(replies)
     if randreply == replies[4]:
         randreply = "Don't bother me - you know for well that I'm doing your Homework,"
-    await ctx.send(randreply + ' ' + name)
+    await ctx.send(randreply + ' ' + name + discrim)
 
 @client.command(description = "tells you the date")
 async def date(ctx):
