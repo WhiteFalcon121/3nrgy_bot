fd13a6a5627133e5623b75fcf6d689fa4fcb9f6d
WhiteFalcon121 <karkau007@gmail.com>
2020-07-21 14:12:20 +0100

    add empty skin check

--- all_functions.py
+++ all_functions.py
@@ -131,10 +131,10 @@ def ask_for_trade_db(ctx, recipient, skin, trade_skin):
     #elif inv_check(person) != 1 or inv_check(person) != 1:
         #return "You need to have inventories to trade."
     person_inv, recipient_inv = read_inv_db(person), read_inv_db(recipient)
-    print(person_inv, recipient_inv)
+    print(person_inv, recipient_inv) # check that skins aren't empty so ""
     if person_inv == 0 and recipient_inv == 0:
         return "Check if you both have inventories."
-    if skin not in person_inv and trade_skin not in recipient_inv:
+    if skin not in person_inv and trade_skin not in recipient_inv and skin not "" and trade_skin not "":
         return "Check both of you have the skins you want to trade."
     result = query_manage("insert into ongoing_trades (person, recipient, skin, trade_skin) VALUES ('{}', '{}', '{}', '{}')".format(person, recipient, skin, trade_skin))
     if result == 1:
